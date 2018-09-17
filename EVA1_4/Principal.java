dc167496393267d15dc43a795225668f541472a1
VCAngel <42756104+VCAngel@users.noreply.github.com>
2018-09-16 22:28:30 -0600

    Prácticas
    Jue

---
+++ EVA1_4/Principal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e;
+
+/**
+ *
+ * @author Vargas Casavantes Angel 
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        short [] tnum = new short[10000000000]; //1GB Ocupa este array
+        System.out.println(tnum);
+        int [] inum = new int [10000000000]; //4G ocupa este array
+        System.out.println(inum);
+    }
+    
+}
